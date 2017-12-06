8cd1eec8990bba87d5328afe6d72c1a1d38e03e6
Tiger <alvietri@gmail.com>
2017-12-06 11:09:34 +0100

    Updated 'select_dataset_svm.py' -> Created 'select', 'createFilesList' and 'parseAnnotation' functions Added 'annotations' and 'images' folders to 'file' folder

--- model_processing/select_dataset_svm.py
+++ model_processing/select_dataset_svm.py
@@ -1,9 +1,95 @@
 import sys
 import os
+from os import listdir
+from os.path import exists, splitext
+from xml.dom import minidom
+
+
+def parseAnnotation(annotation_file, classes_list):
+    xmldoc = minidom.parse(annotation_file)
+    root = xmldoc.getElementsByTagName('annotation')
+    # annotation = {'filename': "NULL", 'folder': "NULL", 'objectscount': 0, 'objects': []}
+    for node in root[0].childNodes:
+        if node.nodeType == node.ELEMENT_NODE:
+            if node.nodeName == "filename":
+                filename = node.firstChild.nodeValue.strip()
+                # annotation['filename'] = filename
+            # elif node.nodeName == "folder":
+            #     folder = node.firstChild.nodeValue.strip()
+            #     annotation['folder'] = folder
+            # elif node.nodeName == "object":
+            elif node.nodeName == "object":
+                # annotation['objectscount'] = annotation['objectscount'] + 1
+                # objectinfo = {'name': "NULL", 'id': "NULL", 'polygon': {'pointscount': 0, 'points': []}}
+                for object in node.childNodes:
+                    if object.nodeType == object.ELEMENT_NODE:
+                        if object.nodeName == "name":
+                            objectname = object.firstChild.nodeValue.strip()
+                            # objectinfo['name'] = objectname
+                            if objectname in classes_list:
+                                # print "Found in: '" + filename + "'"
+                                return True
+                        # elif object.nodeName == "id":
+                        #     objectid = object.firstChild.nodeValue.strip()
+                        #     objectinfo['id'] = objectid
+                #         elif object.nodeName == "polygon":
+                #             for point in object.childNodes:
+                #                 if point.nodeType == point.ELEMENT_NODE:
+                #                     if point.nodeName == "pt":
+                #                         objectinfo['polygon']['pointscount'] = objectinfo['polygon']['pointscount'] + 1
+                #                         pointcoordinates = []
+                #                         for coordinate in point.childNodes:
+                #                             if coordinate.nodeType == coordinate.ELEMENT_NODE:
+                #                                 if coordinate.nodeName == "x":
+                #                                     pointcoordinates.append(coordinate.firstChild.nodeValue.strip())
+                #                                 elif coordinate.nodeName == "y":
+                #                                     pointcoordinates.append(coordinate.firstChild.nodeValue.strip())
+                #                         objectinfo['polygon']['points'].append(pointcoordinates)
+                # annotation['objects'].append(objectinfo)
+    return False
+
+
+def createFilesList(directory):
+    """
+    > createFilesList(directory)
+    Create a list of annotation files that are physically associated with an image file (otherwise, it's impossible
+    to verify the presence or not of the objects)
+
+    :param directory: the source directory
+    :return: a list of files that are physically associated with an image
+    """
+    files = []
+    for dir in listdir(directory + "/images/"):
+        for image in listdir(directory + "/images/" + dir):
+            name = splitext(image)[0]
+            if exists(directory + "/annotations/" + dir + "/" + name + ".xml"):
+                files.append(directory + "/annotations/" + dir + "/" + name + ".xml")
+    return files
 
 
 def select(src_dir, conf_dir, classes_file):
-    print ''
+    # Create usable files list
+    print "---[ Creating usable files list... ]---"
+    usable_files = createFilesList(src_dir)
+    # print files
+    print "---[ Usable files list created ]---\n"
+
+    # Create classes list
+    print "---[ Creating classes list... ]---"
+    with open(classes_file) as f:
+        classes_list = f.readlines()
+    classes_list = [x.strip() for x in classes_list]
+    # print content
+    print "---[ Classes list created ]---\n"
+
+    # Search files containing one of the classes
+    print "---[ Searching for images containing the objects... ]---"
+    containing = []
+    for annotation_file in usable_files:
+        if parseAnnotation(annotation_file, classes_list):
+            containing.append(annotation_file)
+    # print containing
+    print "---[ Search completed ]---\n"
 
 
 if __name__ == '__main__':
